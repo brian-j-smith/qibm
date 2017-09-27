de2b456787f9887213441f16690c944f13d05542
Brian J Smith <brian-j-smith@uiowa.edu>
2017-09-27 10:16:40 -0500

    Implement uniform priors for variance parameters.

--- R/qibm.R
+++ R/qibm.R
@@ -131,16 +131,20 @@ qibm <- function(fixed, image, operator, data, priors = list(),
     Sigma.img.scale <- getprior("Sigma.img.scale", 1.0) *
       (diag(methodN) + getprior("Sigma.img.cor", 0.0) * (1 - diag(methodN)))
     Sigma.img.df <- getprior("Sigma.img.df", methodN)
-    sigma2.opr.shape <- getprior("sigma2.opr.shape", 1.0e-3)
-    sigma2.opr.rate <- getprior("sigma2.opr.rate", 1.0e-3)
-    sigma2.imgopr.shape <- getprior("sigma2.imgopr.shape", 1.0e-3)
-    sigma2.imgopr.rate <- getprior("sigma2.imgopr.rate", 1.0e-3)
-    sigma2.err.shape <- getprior("sigma2.err.shape", 1.0e-3)
-    sigma2.err.rate <- getprior("sigma2.err.rate", 1.0e-3)
     
     gof <- array(0, dim = c(N, methodN, 2))
     Map(function(i) gof[i, methodIDX[i], ] <<- NA, 1:N)
   })
+  for(suffix in c("opr", "imgopr", "err")) {
+    node <- paste("sigma", suffix, "lim", sep=".")
+    if(exists(node, priors)) {
+      model.data[[node]] <- priors[[node]]
+    } else {
+      nodes <- paste("sigma2", suffix, c("shape", "rate"), sep=".")
+      model.data[[nodes[1]]] <- getprior(nodes[1], 1.0e-3)
+      model.data[[nodes[2]]] <- getprior(nodes[2], 1.0e-3)
+    }
+  }
   
   ### Initial values generator
   inits <- function(chain) {
@@ -157,15 +161,28 @@ qibm <- function(fixed, image, operator, data, priors = list(),
                    hw = sqrt(sqrt(prec * 12) / 2))
     stats$prec1 <- pmax(stats$prec - stats$hw, 0)
     stats$prec2 <- stats$prec1 + 2 * stats$hw
-    within(list(), {
-      mu <- rnorm(model.data$methodN, stats$mean, stats$se)
-      tau.imgopr <- rep(NA, max(model.data$methodvarIDX))
-      tau.imgopr[model.data$methodvarIDX] <-
+    
+    val <- list()
+    val$mu <- rnorm(model.data$methodN, stats$mean, stats$se)
+    lim <- model.data$sigma.imgopr.lim
+    if(!is.null(lim)) {
+      val$sigma.imgopr <- rep(NA, max(model.data$methodvarIDX))
+      val$sigma.imgopr[model.data$methodvarIDX] <-
+        runif(model.data$methodvarN, lim[1], lim[2])
+    } else {
+      val$tau.imgopr <- rep(NA, max(model.data$methodvarIDX))
+      val$tau.imgopr[model.data$methodvarIDX] <-
         runif(model.data$methodvarN, stats$prec1, stats$prec2)
-      tau.err <- runif(model.data$methodN, stats$prec1, stats$prec2)
-      .RNG.name <- "base::Wichmann-Hill"
-      .RNG.seed <- chain
-    })
+    }
+    lim <- model.data$sigma.err.lim
+    if(!is.null(lim)) {
+      val$sigma.err <- runif(model.data$methodN, lim[1], lim[2])
+    } else {
+      val$tau.err <- runif(model.data$methodN, stats$prec1, stats$prec2)
+    }
+    val$.RNG.name <- "base::Wichmann-Hill"
+    val$.RNG.seed <- chain
+    val
   }
   
   ### JAGS model specification
@@ -202,16 +219,13 @@ qibm <- function(fixed, image, operator, data, priors = list(),
       }
       
       for (i in 1:methodvarN) {
-        tau.opr[methodvarIDX[i]] ~ dgamma(sigma2.opr.shape, sigma2.opr.rate)
-        sigma.opr[methodvarIDX[i]] <- 1 / sqrt(tau.opr[methodvarIDX[i]])
-        tau.imgopr[methodvarIDX[i]] ~ dgamma(sigma2.imgopr.shape, sigma2.imgopr.rate)
-        sigma.imgopr[methodvarIDX[i]] <- 1 / sqrt(tau.imgopr[methodvarIDX[i]])
+        sigma.opr.prior
+        sigma.imgopr.prior
       }
       
       for (j in 1:methodN) {
         mu[j] ~ dnorm(mu.mean, 1 / mu.var)
-        tau.err[j] ~ dgamma(sigma2.err.shape, sigma2.err.rate)
-        sigma.err[j] <- 1 / sqrt(tau.err[j])
+        sigma.err.prior
         
         gof.rep[j] <- sum(gof[, j, 1]) / n[j]
         gof.obs[j] <- sum(gof[, j, 2]) / n[j]
@@ -220,11 +234,33 @@ qibm <- function(fixed, image, operator, data, priors = list(),
       Omega.img[1:methodN, 1:methodN] ~ dwish(Sigma.img.scale[,], Sigma.img.df)
       Sigma.img[1:methodN, 1:methodN] <- inverse(Omega.img[,])
     }
-  "
+  " %>%
+    gsubprior("opr", "methodvarIDX[i]", model.data) %>%
+    gsubprior("imgopr", "methodvarIDX[i]", model.data) %>%
+    gsubprior("err", "j", model.data)
   
   ### Model fit
   chains <- rjags::jags.model(textConnection(qibm.jags), model.data, inits, n.chains) %>%
     rjags::coda.samples(parameters, n.iter, n.thin) %>%
     window(n.burnin + 1)
   new("qibm", chains, M = model.data$methodN)
+}
+
+
+gsubprior <- function(x, suffix, index, data) {
+  replacement <- 
+    if(exists(paste("sigma2", suffix, "shape", sep="."), data)) {
+      paste0("tau.", suffix, "[", index, "]",
+             " ~ dgamma(sigma2.", suffix, ".shape, sigma2.", suffix, ".rate)\n",
+             "sigma.", suffix, "[", index, "]",
+             " <- 1 / sqrt(tau.", suffix, "[", index, "])")
+    } else if(exists(paste("sigma", suffix, "lim", sep="."), data)) {
+      paste0("tau.", suffix, "[", index, "]",
+             " <- 1 / pow(sigma.", suffix, "[", index, "], 2)\n",
+             "sigma.", suffix, "[", index, "]",
+             " ~ dunif(sigma.", suffix, ".lim[1], sigma.", suffix, ".lim[2])")
+    } else {
+      stop("Unsupported prior for node: sigma.", suffix)
+    }
+  gsub(paste("sigma", suffix, "prior", sep="."), replacement, x)
 }
